403e5e772a3a673b114c36fe3fc15e6494352317
David <david.r.jones@sheffield.ac.uk>
2020-01-08 12:12:09 +0000

    Print a tidier display for reply tweets

--- tweetsort.py
+++ tweetsort.py
@@ -5,20 +5,23 @@ Take tweets as input.
 Sort into "original" and "reply" tweets.
 """
 
+import collections
 import json
 import re
 import sys
+import warnings
 
 JSON = json.JSONDecoder()
 
-def tweets(inp):
+def parse_tweets(inp):
+    """
+    Successively parse complete JSON objects from the entire input.
+    Return a list of those objects.
+    """
     text = inp.read()
     tweets = []
 
     while True:
-        # successively parse complete JSON objects
-        # from the entire input.
-        print(len(tweets))
 
         # Trim initial whitespace,
         # because raw_decode _still_ doesn't handle it.
@@ -35,15 +38,46 @@ def tweets(inp):
         tweets.append(tw)
         text = text[length:]
 
+    return tweets
+
+
+def read_replies(inp):
+    """
+    Returns a pair of:
+        (tweets, replies)
+    Both dicts, indexed by the id_str of the original tweet.
+    """
+    tweets = parse_tweets(inp)
+
+    # All the tweets, indexed by their id_str
+    tweet_dict = dict()
+    # All the replies to a tweet (as a list), indexed by the
+    # id_str of the original tweet (to which the replies are replying).
+    reply_dict = collections.defaultdict(list)
+
     for tweet in tweets:
-        replyto = tweet.get('in_reply_to_status_id_str')
-        print(tweet['id_str'], replyto)
+        tweet_id = tweet['id_str']
+        if tweet_id in tweet_dict:
+            warnings.warn("Tweet id {} has been duplicated".format(tweet_id))
+            continue
+        tweet_dict[tweet_id] = tweet
+
+        reply_to = tweet.get('in_reply_to_status_id_str')
+        if reply_to:
+            reply_dict[reply_to].append(tweet)
+
+    return tweet_dict, reply_dict
+
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
   
-    tweets(sys.stdin)
+    ts, rs, = read_replies(sys.stdin)
+    for original_id, replies in rs.items():
+        tick = original_id in ts
+        for reply in replies:
+            print(reply['id_str'], "replying to", original_id, "✓" if tick else "?")
 
 if __name__ == "__main__":
     main()
