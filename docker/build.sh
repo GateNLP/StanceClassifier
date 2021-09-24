08dba4776b9fad1378e24f9e9f6c7c7d97822406
Mark A. Greenwood <m.a.greenwood@sheffield.ac.uk>
2021-09-24 09:11:20 +0100

    updated the build process to use the new BuildKit backend for Docker. This allows us to provide a cache for the pip install stage making subsequent builds much faster without increasing the size of the final images

--- docker/build.sh
+++ docker/build.sh
@@ -4,4 +4,4 @@ if [ -z "$TAG" ]; then
   TAG=elg/stance_$1:latest
 fi
 
-exec docker build -f Dockerfile -t $TAG --build-arg LANGUAGE=$1 ..
+exec docker buildx build -f Dockerfile -t $TAG --build-arg LANGUAGE=$1 ..
