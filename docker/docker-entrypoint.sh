60e885ef939617d245a306d28fcca4b27846428d
Ian Roberts <i.roberts@dcs.shef.ac.uk>
2021-10-13 16:50:02 +0000

    Added a hook to allow injection of setup scripts, e.g. to set environment variables dynamically based on characteristics of the host that is running the container

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+# Allow for setup shell scripts to be injected to set up environment variables
+# etc. before we execute the main process
+for f in setup.d/*.sh ; do
+  if [ -r "$f" ]; then
+    . "$f"
+  fi
+done
+
 exec /sbin/tini -- venv/bin/gunicorn -b=0.0.0.0:8000 "--workers=$WORKERS" "$@" elg_stance:app
